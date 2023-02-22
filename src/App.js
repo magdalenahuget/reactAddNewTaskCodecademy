56e747a7da15e3471921c127b0c6ac2d5f9f5b80
magdalenahuget <magdalena.huget@gmail.com>
2023-02-22 23:50:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from "react";
+// import NewTask from "../Presentational/NewTask";
+// import TasksList from "../Presentational/TasksList";
 
-function App() {
+// export default function AppFunction() {
+//   const [newTask, setNewTask] = useState({});
+//   const handleChange = ({ target }) => {
+//     const { name, value } = target;
+//     setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
+//   };
+
+//   const [allTasks, setAllTasks] = useState([]);
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     if (!newTask.title) return;
+//     setAllTasks((prev) => [newTask, ...prev]);
+//     setNewTask({});
+//   };
+//   const handleDelete = (taskIdToRemove) => {
+//     setAllTasks((prev) => prev.filter((task) => task.id !== taskIdToRemove));
+//   };
+
+//   return (
+//     <main>
+//       <h1>Tasks</h1>
+//       <NewTask
+//         newTask={newTask}
+//         handleChange={handleChange}
+//         handleSubmit={handleSubmit}
+//       />
+//       <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+//     </main>
+//   );
+// }
+
+
+// import the default export and the named export `useState` from the 'react' library
+import React, {useState } from 'react';
+
+export default function ColorPicker() {
+  // call useState and assign its return values to `color` and `setColor`
+const [color, setColor] = useState();
+
+ const divStyle = {backgroundColor: color};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle}>
+      <p>The color is {color}</p>
+      <button onClick={() => setColor("Aquamarine")}>
+        Aquamarine
+      </button>
+      <button onClick={() => setColor("BlueViolet")}>
+        BlueViolet
+      </button>
+      <button onClick={() => setColor('Chartreuse')}>
+        Chartreuse
+      </button>
+      <button onClick={() => setColor('CornflowerBlue')}>
+        CornflowerBlue
+      </button>
     </div>
   );
 }
-
-export default App;
